ff450a64847046bc2f7adc68eed3f0c8e2ff7b19
Nil <nilyos01@easv365.dk>
2024-03-06 22:04:54 +0100

    first commit

--- src/dk/easv/bll/bot/ExampleSneakyBot.java
+++ src/dk/easv/bll/bot/ExampleSneakyBot.java
@@ -1,6 +1,5 @@
 package dk.easv.bll.bot;
 
-import dk.easv.bll.bot.IBot;
 import dk.easv.bll.field.IField;
 import dk.easv.bll.game.GameState;
 import dk.easv.bll.game.IGameState;
@@ -8,6 +7,8 @@ import dk.easv.bll.move.IMove;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
+
+import static dk.easv.bll.game.GameManager.isWin;
 
 public class ExampleSneakyBot implements IBot {
     final int moveTimeMs = 1000;
@@ -26,58 +27,283 @@ public class ExampleSneakyBot implements IBot {
 
     @Override
     public IMove doMove(IGameState state) {
-        return calculateWinningMove(state, moveTimeMs);
-    }
-    // Plays single games until it wins and returns the first move for that. If iterations reached with no clear win, just return random valid move
-    private IMove calculateWinningMove(IGameState state, int maxTimeMs){
-        long time = System.currentTimeMillis();
-        Random rand = new Random();
-        int count = 0;
-        while (System.currentTimeMillis() < time + maxTimeMs) { // check how much time has passed, stop if over maxTimeMs
-            GameSimulator simulator = createSimulator(state);
-            IGameState gs = simulator.getCurrentState();
-            List<IMove> moves = gs.getField().getAvailableMoves();
-            IMove randomMovePlayer = moves.get(rand.nextInt(moves.size()));
-            IMove winnerMove = randomMovePlayer;
-
-            while (simulator.getGameOver()==GameOverState.Active){ // Game not ended
-                simulator.updateGame(randomMovePlayer);
-
-                // Opponent plays randomly
-                if (simulator.getGameOver()==GameOverState.Active){ // game still going
-                    moves = gs.getField().getAvailableMoves();
-                    IMove randomMoveOpponent = moves.get(rand.nextInt(moves.size()));
-                    simulator.updateGame(randomMoveOpponent);
-                }
-                if (simulator.getGameOver()==GameOverState.Active){ // game still going
-                    moves = gs.getField().getAvailableMoves();
-                    randomMovePlayer = moves.get(rand.nextInt(moves.size()));
-                }
-            }
-
-            if (simulator.getGameOver()==GameOverState.Win){
-                //System.out.println("Found a win, :)");
-                return winnerMove; // Hint you could maybe save multiple games and pick the best? Now it just returns at a possible victory
-            }
-            count++;
+        if (state.getMoveNumber() == 0) {
+            // Select a random initial move if it's the first move of the game
+            return selectInitialMove(state);
+        } else {
+            // Otherwise, calculate the winning move
+            return calculateWinningMove(state, moveTimeMs);
         }
-        //System.out.println("Did not win, just doing random :¨(");
-        List<IMove> moves = state.getField().getAvailableMoves();
-        IMove randomMovePlayer = moves.get(rand.nextInt(moves.size()));
-        return randomMovePlayer; // just play randomly if solution not found
     }
 
-    /*
-        The code below is a simulator for simulation of gameplay. This is needed for AI.
+    private IMove selectInitialMove(IGameState state) {
+        List<IMove> availableMoves = state.getField().getAvailableMoves();
 
-        It is put here to make the Bot independent of the GameManager and its subclasses/enums
+        // Prioritize the center of the board as the initial move
+        for (IMove move : availableMoves) {
+            if (move.getX() == 4 && move.getY() == 4) {
+                return move;
+            }
+        }
 
-        Now this class is only dependent on a few interfaces: IMove, IField, and IGameState
+        // If the center is not available, choose a random move
+        Random rand = new Random();
+        return availableMoves.get(rand.nextInt(availableMoves.size()));
+    }
 
-        You could say it is self-contained. The drawback is that if the game rules change, the simulator must be
-        changed accordingly, making the code redundant.
 
-     */
+    private IMove calculateWinningMove(IGameState state, int maxTimeMs) {
+        long startTime = System.currentTimeMillis();
+        Random rand = new Random();
+
+        // Get the opponent's player ID
+        int opponentPlayer = (state.getMoveNumber() + 1) % 2;
+
+        List<IMove> availableMoves = state.getField().getAvailableMoves();
+        IMove bestMove = null;
+        int bestScore = Integer.MIN_VALUE;
+
+        // Iterate through all available moves
+        for (IMove move : availableMoves) {
+            GameSimulator simulator = createSimulator(state);
+            simulator.updateGame(move); // Make the move
+
+            // Check if the opponent wins after this move
+            if (isWinForPlayer(simulator.getCurrentState(), opponentPlayer)) {
+                return move; // Return the move to block the opponent's win
+            }
+
+            // Check if the opponent has a winning move next
+            boolean opponentHasWinningMove = hasPotentialWin(simulator.getCurrentState(), opponentPlayer);
+            int score = evaluateGameState(simulator.getCurrentState(), state.getMoveNumber() % 2);
+
+            // Prioritize moves that block the opponent's potential winning moves
+            if (!opponentHasWinningMove) {
+                score += 10; // Give a bonus score to moves that don't allow the opponent to win next turn
+            }
+
+            // Update the best move if necessary
+            if (score > bestScore) {
+                bestMove = move;
+                bestScore = score;
+            }
+
+            // Check if the time limit has been reached
+            if (System.currentTimeMillis() - startTime >= maxTimeMs) {
+                break; // Exit the loop if time limit exceeded
+            }
+        }
+
+        // If no winning move by the opponent is found, return the best move based on evaluation function
+        if (bestMove != null) {
+            return bestMove;
+        }
+
+        // If no moves were evaluated, return a random move
+        return availableMoves.get(rand.nextInt(availableMoves.size()));
+    }
+
+    private boolean hasPotentialWin(IGameState state, int player) {
+        List<IMove> availableMoves = state.getField().getAvailableMoves();
+        for (IMove move : availableMoves) {
+            GameSimulator simulator = createSimulator(state);
+            simulator.updateGame(move);
+            if (isWinForPlayer(simulator.getCurrentState(), player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private boolean isWinForPlayer(IGameState state, int player) {
+        String[][] board = state.getField().getBoard();
+        String[][] macroboard = state.getField().getMacroboard();
+
+        // Check for wins in the microboards
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                // Check each microboard for a win
+                if (isMicroboardWin(board, i, j, player)) {
+                    int macroX = i / 3;
+                    int macroY = j / 3;
+                    // Check if the macroboard itself leads to a win
+                    if (isWin(macroboard, new Move(macroX, macroY), Integer.toString(player))) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false; // No win found
+    }
+
+    private boolean isMicroboardWin(String[][] board, int startX, int startY, int player) {
+        // Check horizontal, vertical, and diagonal wins within a microboard
+        for (int i = startX; i < startX + 3; i++) {
+            for (int j = startY; j < startY + 3; j++) {
+                if (board[i][startY].equals(Integer.toString(player)) &&
+                        board[i][startY + 1].equals(Integer.toString(player)) &&
+                        board[i][startY + 2].equals(Integer.toString(player))) {
+                    return true; // Horizontal win
+                }
+
+                if (board[startX][j].equals(Integer.toString(player)) &&
+                        board[startX + 1][j].equals(Integer.toString(player)) &&
+                        board[startX + 2][j].equals(Integer.toString(player))) {
+                    return true; // Vertical win
+                }
+            }
+        }
+
+        // Check diagonals
+        if (board[startX][startY].equals(Integer.toString(player)) &&
+                board[startX + 1][startY + 1].equals(Integer.toString(player)) &&
+                board[startX + 2][startY + 2].equals(Integer.toString(player))) {
+            return true; // Diagonal win
+        }
+
+        if (board[startX][startY + 2].equals(Integer.toString(player)) &&
+                board[startX + 1][startY + 1].equals(Integer.toString(player)) &&
+                board[startX + 2][startY].equals(Integer.toString(player))) {
+            return true; // Diagonal win
+        }
+
+        return false; // No win found within the microboard
+    }
+    private int evaluateGameState(IGameState state, int player) {
+        int score = 0;
+
+        // Evaluate the control of microboards
+        score += evaluateMicroboards(state, player);
+
+        // Evaluate the proximity to winning moves
+        score += evaluateWinningMoves(state, player);
+
+        // Evaluate the blocking of opponent's moves
+        score += evaluateBlockingOpponentMoves(state, player);
+
+        return score;
+    }
+
+    private int evaluateMicroboards(IGameState state, int player) {
+        int playerScore = 0;
+        int opponentScore = 0;
+
+        String[][] macroboard = state.getField().getMacroboard();
+
+        // Check control of microboards
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                int microboardScore = evaluateMicroboard(state, player, i, j);
+                if (macroboard[i / 3][j / 3].equals(Integer.toString(player))) {
+                    playerScore += microboardScore;
+                } else {
+                    opponentScore += microboardScore;
+                }
+            }
+        }
+
+        return playerScore - opponentScore;
+    }
+
+    private int evaluateMicroboard(IGameState state, int player, int startX, int startY) {
+        String[][] board = state.getField().getBoard();
+
+        // Evaluate control of a single microboard
+        int playerCount = 0;
+        int opponentCount = 0;
+
+        for (int i = startX; i < startX + 3; i++) {
+            for (int j = startY; j < startY + 3; j++) {
+                if (board[i][j].equals(Integer.toString(player))) {
+                    playerCount++;
+                } else if (!board[i][j].equals(IField.EMPTY_FIELD)) {
+                    opponentCount++;
+                }
+            }
+        }
+
+        // Assign scores based on control
+        if (playerCount > opponentCount) {
+            return 1;
+        } else if (playerCount < opponentCount) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    private int evaluateWinningMoves(IGameState state, int player) {
+        int winningMoves = 0;
+
+        // Evaluate rows
+        for (int i = 0; i < 9; i++) {
+            if (hasPotentialWin(state, player, i, 0, 0, 1)) {
+                winningMoves++;
+            }
+        }
+
+        // Evaluate columns
+        for (int j = 0; j < 9; j++) {
+            if (hasPotentialWin(state, player, 0, j, 1, 0)) {
+                winningMoves++;
+            }
+        }
+
+        // Evaluate diagonals
+        if (hasPotentialWin(state, player, 0, 0, 1, 1)) {
+            winningMoves++;
+        }
+        if (hasPotentialWin(state, player, 0, 2, 1, -1)) {
+            winningMoves++;
+        }
+
+        return winningMoves;
+    }
+
+    private boolean hasPotentialWin(IGameState state, int player, int startX, int startY, int dx, int dy) {
+        String[][] board = state.getField().getBoard();
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            int x = startX + i * dx;
+            int y = startY + i * dy;
+            if (board[x][y].equals(Integer.toString(player)) || board[x][y].equals(IField.EMPTY_FIELD)) {
+                count++;
+            }
+        }
+        return count == 2;
+    }
+
+
+    private int evaluateBlockingOpponentMoves(IGameState state, int player) {
+        int blockingMoves = 0;
+
+        // Evaluate rows
+        for (int i = 0; i < 9; i++) {
+            if (hasPotentialWin(state, 1 - player, i, 0, 0, 1)) {
+                blockingMoves++;
+            }
+        }
+
+        // Evaluate columns
+        for (int j = 0; j < 9; j++) {
+            if (hasPotentialWin(state, 1 - player, 0, j, 1, 0)) {
+                blockingMoves++;
+            }
+        }
+
+        // Evaluate diagonals
+        if (hasPotentialWin(state, 1 - player, 0, 0, 1, 1)) {
+            blockingMoves++;
+        }
+        if (hasPotentialWin(state, 1 - player, 0, 2, 1, -1)) {
+            blockingMoves++;
+        }
+
+        return blockingMoves;
+    }
+
 
     @Override
     public String getBotName() {
@@ -164,7 +390,7 @@ public class ExampleSneakyBot implements IBot {
         }
 
         private Boolean verifyMoveLegality(IMove move) {
-            IField field = currentState.getField();
+                IField field = currentState.getField();
             boolean isValid = field.isInActiveMicroboard(move.getX(), move.getY());
 
             if (isValid && (move.getX() < 0 || 9 <= move.getX())) isValid = false;
@@ -297,3 +523,4 @@ public class ExampleSneakyBot implements IBot {
     }
 
 }
+
